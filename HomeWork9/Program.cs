8b155f97a3d7ada83be9b612de538f32a7d14ea9
Elene <L.Rokolunskaya@Gmail.com>
2022-07-24 20:43:02 +0300

    HomeWork9

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.N = 4532 -> 4
+/*int Amountnum (int N)
+{
+   if(N<=9)
+    {
+     return 1;
+    }
+    else return 1+ Amountnum(N/10);
+}
+Console.WriteLine( Amountnum(2507));// Цифра в скобках для примера.*/
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int Sumnum( int M, int N)
+{
+    if(M==N)
+    {
+     return N;
+        }
+        else
+        {
+         return  N+ Sumnum(M,N-1);   
+        }
+}
+ Console.WriteLine(Sumnum(3,5));
